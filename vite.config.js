73440f92bd6e8251331b4cd6cc908fbe7a06e117
Admin <erarvinder2012@gmail.com>
2025-11-12 21:57:12 +0200

    CONFIG UP

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: process.env.PORT || 10000,
     allowedHosts: [
-      'frontend-oauth-direct.onrender.com'
+      'scpms-frontend.onrender.com'
     ]
   }
 })
